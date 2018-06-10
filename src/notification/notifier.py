34b25ba2f2a232e9c95cf5c82b3d737ff04c9744
Ruihao Huang <huangruihaocst@126.com>
2018-06-10 09:28:46 +0800

    Added paper directory.

--- src/notification/notifier.py
+++ src/notification/notifier.py
@@ -7,10 +7,10 @@ PORT = 25
 
 class Notifier:
 
-    def __int__(self):
+    def __init__(self):
         with open('../config.json') as f:
             config = json.loads(f.read())
-        self.__emails = config['emails']
+        self.__emails = config['notification']['emails']
 
     def notify(self, total, specials):
         content = '本次威胁情报聚合完成，您有' + str(total) + '台主机可能受到威胁，'
@@ -18,19 +18,18 @@ class Notifier:
             content += '其中您关注的'
         for cve in specials:
             content += cve['name'] + '有' + str(cve['count']) + '台主机可能受到威胁，'
-        content += '可以前往xxx查看。'
+        content += '可以前往202.112.51.123:5000查看。'
         print(content)
         for recipient in self.__emails:
             msg = MIMEText(content)
-            msg['Subject'] = '扫描完成！'
+            msg['Subject'] = '分析完成！'
             msg['From'] = 'admin@threats-aggregation.com'
-            msg['To'] = recipient.split('@')[1]
-            s = smtplib.SMTP(recipient, PORT)
+            msg['To'] = recipient
+            s = smtplib.SMTP(recipient.split('@')[1], PORT)
             s.send_message(msg)
             s.quit()
 
 
 if __name__ == '__main__':
-    # notifier = Notifier()
-    # notifier.notify()
-    pass
+    notifier = Notifier()
+    notifier.notify(45, [{'name': 'CVE-2018-0171', 'count': 1}])
